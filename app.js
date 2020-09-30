52435d931c697097303b31951d6889a798b13b37
florez18399 <andresfflorez1999@hotmail.com>
2020-09-29 20:44:07 -0500

    Procesamiento de imagenes de manera paralela

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    getColorPixels().then(async(colorPixels) => {
+    getColorPixels('thumbnail-at-292.8024-seconds.png').then(async (colorPixels) => {
         let majorColors = await getMajorColors(colorPixels)
-        console.log(majorColors); 
+        console.log(majorColors);
     })
 })
 
 app.get('/thumbnail', (req, res) => {
-    generateThumbnail()
+    generateThumbnail((newFiles) => {
+        processFiles = []
+        let majorColors = []
+        for (let i = 0; i < newFiles.length; i++) {
+            processFiles.push(getColorPixels(newFiles[i]))
+        }
+        Promise.all(processFiles).then(async values => {
+            majorColors = await getAllMajor(values)
+            console.log(majorColors);
+        })
+        console.log(':)' + majorColors);
+    })
     res.send('Thumbnail obtenido')
 })
 
@@ -24,8 +35,9 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-function generateThumbnail() {
+function generateThumbnail(cb) {
     console.log('Generando miniaturas')
+    let newFiles = []
     var proc = new ffmpeg(path.join(__dirname, '/public/videos/1_1080.mp4'))
         .screenshots({
             timestamps: ['20%', '40%', '60%', '80%', '99%'],
@@ -33,17 +45,20 @@ function generateThumbnail() {
             folder: path.join(__dirname, '/public/images'),
             size: '200x140'
         }).on('filenames', function (filenames) {
-            console.log('Archivos obtenidos: ' + filenames.join(', '))
+            newFiles = filenames
+            console.log('Archivos obtenidos: ' + newFiles)
         })
         .on('end', function () {
             console.log('Proceso completado');
+            cb(newFiles)
         })
 }
 
-function getColorPixels() {
+function getColorPixels(file) {
+    console.log('Obteniendo el color rgb de cada pixel');
     let colors = []
     let promise = new Promise((resolve, rejects) => {
-        jimp.read(path.join(__dirname, '/public/images/thumbnail-at-25-seconds.png'), (err, image) => {
+        jimp.read(path.join(__dirname, '/public/images/', file), (err, image) => {
             for (let i = 0; i < image.getWidth(); i++) {
                 for (let j = 0; j < image.getHeight(); j++) {
                     const color = jimp.intToRGBA(image.getPixelColor(i, j))
@@ -57,6 +72,7 @@ function getColorPixels() {
 }
 
 function getMajorColors(colorPixels) {
+    console.log('Obteniendo los 10 colores que mas se encuentran en la imagen');
     let promise = new Promise((resolve, reject) => {
         let toCount = function (acc, color) {
             if (!acc[color]) {
@@ -71,8 +87,17 @@ function getMajorColors(colorPixels) {
         let order = Object.keys(colorsCount).sort(function (a, b) {
             return colorsCount[b] - colorsCount[a];
         });
-        order = order.slice(0, 10)  
+        order = order.slice(0, 10)
+        console.log('Lista ordenada: ', order)
         resolve(order)
     })
     return promise
+}
+
+function getAllMajor(listImagesPixels) {
+    getMajorsPromises = []
+    listImagesPixels.forEach(element => {
+        getMajorsPromises.push(getMajorColors(element))
+    });
+    return Promise.all(getMajorsPromises)
 }
